86a2cebda3909aee0e94201df7f733e204823602
Quentin Rasmont <qrasmont@gmail.com>
2023-04-01 17:33:06 +0200

    Initial setup
    Add dependencies to get a buildable firmware for RP2040

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+#![no_std]
+#![no_main]
+
+use defmt::*;
+use defmt_rtt as _;
+use panic_probe as _;
+use rp2040_hal::entry;
+
+#[entry]
+fn main() -> ! {
+    info!("Start");
+
+    loop {
+        info!("loop");
+    }
+}
